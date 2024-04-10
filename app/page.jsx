b8040d857ed0bf5bd553bc959b452923a4a7b13e
iamkhushbooy <khushbooygkp@gmail.com>
2024-04-10 13:48:16 +0530

    changed

--- app/page.jsx
+++ app/page.jsx
@@ -2,10 +2,11 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div className='w-full h-screen bg-[#0a154b]'>
-      <h1 className='text-[white]'> khushboo</h1>
-     
-      
+    <div className='w-full h-screen bg-[#0a154b] flex justify-center items-center'>
+     <div className='w-[70%] h-[50%] border rounded-xl flex justify-around items-center flex-col'> 
+      <textarea className='w-[70%] h-[20%] rounded p-3' name="" id="" cols="30" rows="10"></textarea>
+      <input className='w-[70%] h-[12%] rounded p-3' type="text" placeholder='email'/>
+      <button className='w-[70%] h-[12%] bg-[#08088e] rounded'>Send</button></div>
     </div>
   )
 }
