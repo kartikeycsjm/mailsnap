97599e3d0493eb0855f4419aeb9068606a7d8afe
kartikeycsjm <kartikeymishracsjm@gmail.com>
2024-04-10 16:17:52 +0530

    hello

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import Message from './components/Message';
-
 const Page = () => {
   return (
     <div className='w-full h-screen bg-[#0a154b] flex justify-around items-center flex-col'>
@@ -9,7 +8,7 @@ const Page = () => {
         <input className='w-[70%] h-[12%] rounded p-2' type="text" placeholder='email' />
         <button className='w-[70%] h-[12%] bg-[#08088e] rounded'>Send</button>
       </div>
-      <div className='w-[95%] h-[300px] overflow-x-auto'>
+      <div className='h-[300px] overflow-x-auto'>
         <div className='flex'>
           <Message />
           <Message />
