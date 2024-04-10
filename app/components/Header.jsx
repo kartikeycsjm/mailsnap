47096983774a1b5b4fdc772c468a93cb84ced830
kartikeycsjm <kartikeymishracsjm@gmail.com>
2024-04-10 12:58:32 +0530

    mailsnap

---
+++ app/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div className='w-full h-[70px] flex justify-around items-center bg-[#0b1234] text-[white]'>
+            <h1 className='text-[26px] w-[30%] font-[cursive] flex justify-center text-[#aace18]' >mailsnap</h1>
+            <div className='w-[65%] flex justify-around'>
+                <input className='w-[70%] h-[36px] bg- rounded-lg p-2 border-black' type="search" placeholder='search email' />
+                <button className='w-[25%] h-[36px] bg-[#08088e] rounded-xl'>Search</button>
+            </div>
+        </div>
+    )
+}
+
+export default Header
---
+++ app/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='w-full h-screen bg-[#0a154b]'>
+     
+      
+    </div>
+  )
+}
+
+export default page
