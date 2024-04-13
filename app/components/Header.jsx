553ea136bd849d691ec41f16a054781a43452706
kartikeycsjm <kartikeymishracsjm@gmail.com>
2024-04-13 14:03:34 +0530

    added webkit scroller

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -4,7 +4,7 @@ const Header = () => {
         <div className='w-full h-[70px] flex justify-around items-center bg-[#0b1234] text-[white]'>
             <h1 className='text-[38px] w-[30%] font-[cursive] flex justify-center text-[#e57343] items-center' >mailsnap</h1>
             <div className='w-[65%] flex justify-around'>
-                <input className='w-[70%] text-black h-[36px] bg- rounded-lg p-2 border-black' type="search" placeholder='search email' value={'hello world khushboo'} />
+                <input className='w-[70%] text-black h-[36px] bg- rounded-lg p-2 border-black' type="search" placeholder='search email'/>
                 <button className='w-[25%] h-[36px] bg-[#08088e] rounded-xl'>Search</button>
             </div>
         </div>
--- app/page.jsx
+++ app/page.jsx
@@ -1,25 +1,31 @@
 import React from 'react';
-import Message from './components/Message';
 
 const Page = () => {
-
   return (
-    <div className='w-full h-screen bg-[#0a154b] flex justify-around items-center flex-col'>
-      <div className='w-[70%] h-[50%] border rounded-xl flex justify-around items-center flex-col'>
-        <textarea className='w-[70%] h-[20%] rounded p-2 resize-none' placeholder='Your Message' name="" id="" cols="30" rows="10"></textarea>
-        <input className='w-[70%] h-[12%] rounded p-2' type="text" placeholder='email' />
-        <button className='w-[70%] h-[12%] bg-[#08088e] rounded'>Send</button>
+    <div className='w-full min-h-screen bg-[#0a154b] flex justify-around items-center flex-col'>
+      <div className='w-[70%] h-[320px] rounded-xl flex justify-center items-center flex-col'>
+        <textarea className='w-[80%] h-[200px] rounded p-2 resize-none' placeholder='Your Message' name="" id="" cols="30" rows="10"></textarea>
+        <input className='m-3 w-[80%] h-[40px] rounded p-2' type="text" placeholder='email' />
+        <button className='w-[80%] h-[12%] bg-[#08088e] rounded'>Send</button>
       </div>
-      <div className='w-[95%] h-[300px] overflow-x-auto'>
-        <div className='flex'>
-          <Message />
-          <Message />
-          <Message />
-          <Message />
+      <div id='scrollable'>
+        <div className="flex whitespace-no-wrap">
+          <div className="d"></div>
+          <div className="d"></div>
+          <div className="d"></div>
+          <div className="d"></div>
+          <div className="d"></div>
+          <div className="d"></div>
+          <div className="d"></div>
+          <div className="d"></div>
+          <div className="d"></div>
+          <div className="d"></div>
+          <div className="d"></div>
+          <div className="d"></div>
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-  export default Page;
+export default Page;
