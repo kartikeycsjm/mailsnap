f1608a82168c290e7d5725aee6b69b770658043e
iamkhushbooy <khushbooygkp@gmail.com>
2024-04-13 15:45:06 +0530

    changed

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 const Header = () => {
     return (
-        <div className='w-full h-[70px] flex justify-around items-center bg-[#0b1234] text-[white]'>
-            <h1 className='text-[38px] w-[30%] font-[cursive] flex justify-center text-[#e57343] items-center' >mailsnap</h1>
-            <div className='w-[65%] flex justify-around'>
-                <input className='w-[70%] text-black h-[36px] bg- rounded-lg p-2 border-black' type="search" placeholder='search email'/>
-                <button className='w-[25%] h-[36px] bg-[#08088e] rounded-xl'>Search</button>
+        <div className='w-full h-[70px] flex justify-around items-center bg-[#17225b] text-[white]'>
+            <h1 className='text-[22px] w-[30%] font-[cursive] flex justify-center text-[#e57343] items-center  sm:text-[28px] md:text-[38px]' >mailsnap</h1>
+            <div className='w-[70%] flex justify-around items-center'>
+                <input className=' w-[70%] text-black h-[31px]  bg- rounded-lg p-2 border-black sm:h-[32px] md:h-[36px]' type="search" placeholder='search email'/>
+                <button className='w-[23%] h-[31px]  bg-[#08088e] rounded-xl sm:h-[32px] md:h-[36px]'>Search</button>
             </div>
         </div>
     )
--- app/page.jsx
+++ app/page.jsx
@@ -1,8 +1,7 @@
 import React from 'react';
-
 const Page = () => {
   return (
-    <div className='w-full min-h-screen bg-[#0a154b] flex justify-around items-center flex-col'>
+    <div className='w-full min-h-screen bg- flex justify-around items-center flex-col'>
       <div className='w-[70%] h-[320px] rounded-xl flex justify-center items-center flex-col'>
         <textarea className='w-[80%] h-[200px] rounded p-2 resize-none' placeholder='Your Message' name="" id="" cols="30" rows="10"></textarea>
         <input className='m-3 w-[80%] h-[40px] rounded p-2' type="text" placeholder='email' />
